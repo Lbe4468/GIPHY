af62d081b2e42032afa00ab05d5290c6273bc1aa
Lucas Epps <lucasbepps@gmail.com>
2018-03-01 16:49:19 -0700

    updates

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,10 +1,15 @@
-var topics = ["dinosaurs", "lizards", "unicorns", "dragons", "sharks"];
-
+// create array to store animals
+var topics = ["DINOSAURS", "LIZARDS", "UNICORNS", "DRAGONS", "SHARKS"];
+// call function to create buttons from array items
+createButtons();
+// function to clear input field after submit
 function clearField() {
   $("#animal-input").val("");
 }
-
+// function to create buttons from array items
 function createButtons() {
+  $("#animalButtons").empty();
+	// loop for array items
   for (var i = 0; i < topics.length; i++) {
     $("<button>")
       .attr("id", topics[i])
@@ -12,32 +17,25 @@ function createButtons() {
       .text(topics[i])
       .appendTo("#animalButtons");
   }
-
-  $("#addAnimal").on("click", function() {
-    event.preventDefault();
-
-    var animalInput = $("#animal-input").val();
-    topics.push(animalInput);
-
-    $("<button>")
-      .attr("id", topics[i])
-      .attr("animal-data", topics[i])
-      .text(topics[i])
-      .appendTo("#animalButtons");
-    console.log(animalInput);
-    console.log(topics);
-
-    clearField();
-  });
 }
-createButtons();
-
-$("button").on("click", function() {
+// function to add text from input field to array upon submit click
+$("#addAnimal").on("click", function(event) {
+  event.preventDefault();
+  var animalInput = $("#animal-input")
+    .val()
+    .trim();
+  topics.push(animalInput);
+  console.log(topics);
+  clearField();
+  createButtons();
+});
+// function and ajax call to pull gif results upon button click and assign designated attributes to results and add images to HTML
+$(document).on("click", "button", function() {
   var animals = $(this).attr("animal-data");
   var queryURL =
     "https://api.giphy.com/v1/gifs/search?q=" +
     animals +
-    "&api_key=dc6zaTOxFJmzC&limit=100";
+    "&api_key=dc6zaTOxFJmzC&limit=10";
 
   $.ajax({
     url: queryURL,
@@ -46,24 +44,22 @@ $("button").on("click", function() {
     var results = response.data;
     for (var i = 0; i < results.length; i++) {
       var gifDiv = $("<div class='item'>");
-
       var rating = results[i].rating;
-
       var p = $("<p>").text("Rating: " + rating);
-
       var animalImage = $("<img>");
-			animalImage.attr("src", results[i].images.fixed_height.url);
-			animalImage.attr("data-state", "animate");
-
+      animalImage.attr("src", results[i].images.fixed_height_still.url);
+      animalImage.attr("data-still", results[i].images.fixed_height_still.url);
+      animalImage.attr("data-animate", results[i].images.fixed_height.url);
+      animalImage.attr("data-state", "still");
+      animalImage.attr("class", "gif");
       gifDiv.prepend(p);
       gifDiv.prepend(animalImage);
-
       $("#animals").prepend(gifDiv);
     }
   });
 });
-
-$(".item").on("click", function() {
+// function that changes gif from still to animated version upon click
+$(document).on("click", ".gif", function() {
   var state = $(this).attr("data-state");
   if (state === "still") {
     $(this).attr("data-state", "animate");
